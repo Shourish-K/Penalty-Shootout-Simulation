be13c5e841222ac2e0a3e78657f35c8c00a0c3d5
Shourish Kothawale <shourish123@gmail.com>
2024-12-09 00:40:06 -0500

    Finished Report

--- Scripts/CSCI_B550_Final_Project_Queries.sql
+++ Scripts/CSCI_B550_Final_Project_Queries.sql
@@ -21,3 +21,17 @@ ORDER BY SHO DESC
 SELECT TOP 1 [Index], Position, Name
 FROM Goalkeepers
 ORDER BY SHO
+
+-- Select Man Utd team with descending order of OVR
+
+SELECT TOP 10 [Index], Position, Name
+FROM ShotTakers
+WHERE Team = 'Man Utd'
+ORDER BY OVR DESC
+
+-- Select Man Utd team with descending order of Penalties
+
+SELECT TOP 10 [Index], Position, Name
+FROM ShotTakers
+WHERE Team = 'Man Utd'
+ORDER BY Penalties DESC
